a0653b241c748dd03a44a1c1ebeeebb2c2d71cac
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-06 12:30:11 +0800

    Update Taiko.sh

--- Taiko.sh
+++ Taiko.sh
@@ -6,10 +6,12 @@ sudo apt update
 # 检查 Git 是否已安装
 if ! command -v git &> /dev/null
 then
-    echo "Git未安装，执行安装..."
+    # 如果 Git 未安装，则进行安装
+    echo "未检测到 Git，正在安装..."
     sudo apt install git -y
 else
-    echo "Git已经安装."
+    # 如果 Git 已安装，则不做任何操作
+    echo "Git 已安装。"
 fi
 
 # 克隆 Taiko 仓库
@@ -24,10 +26,10 @@ if [ ! -f .env ]; then
 fi
 
 # 提示用户输入环境变量的值
-read -p "输入BlockPI holesky HTTP链接: " l1_endpoint_http
-read -p "输入BlockPI holesky WS链接: " l1_endpoint_ws
-read -p "确认是否作为证明者（输入True或者False）: " enable_prover
-read -p "输入0x开头的EVM钱包私钥: " l1_prover_private_key
+read -p "请输入BlockPI holesky HTTP链接: " l1_endpoint_http
+read -p "请输入BlockPI holesky WS链接: " l1_endpoint_ws
+read -p "请确认是否作为证明者（输入True或者False）: " enable_prover
+read -p "请输入0x开头的EVM钱包私钥: " l1_prover_private_key
 
 # 将用户输入的值写入.env文件
 sed -i "s|L1_ENDPOINT_HTTP=.*|L1_ENDPOINT_HTTP=${l1_endpoint_http}|" .env
@@ -35,22 +37,23 @@ sed -i "s|L1_ENDPOINT_WS=.*|L1_ENDPOINT_WS=${l1_endpoint_ws}|" .env
 sed -i "s|ENABLE_PROVER=.*|ENABLE_PROVER=${enable_prover}|" .env
 sed -i "s|L1_PROVER_PRIVATE_KEY=.*|L1_PROVER_PRIVATE_KEY=${l1_prover_private_key}|" .env
 
-# 提示信息
-echo "用户信息已经配置。"
+# 用户信息已配置完毕
+echo "用户信息已配置完毕。"
 
 # 升级所有已安装的包
 sudo apt upgrade -y
 
-# 安装基本组件，包括编译工具、库和其他必要的软件包
+# 安装基本组件
 sudo apt install pkg-config curl build-essential libssl-dev libclang-dev ufw -y
 
 # 检查 Docker 是否已安装
 if ! command -v docker &> /dev/null
 then
-    echo "Docker未安装，执行安装..."
+    # 如果 Docker 未安装，则进行安装
+    echo "未检测到 Docker，正在安装..."
     sudo apt-get install ca-certificates curl gnupg lsb-release
 
-    # 添加 Docker 的官方 GPG 密钥
+    # 添加 Docker 官方 GPG 密钥
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
@@ -59,28 +62,28 @@ then
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    # 为 Docker 文件授权，以防万一，然后更新包索引
+    # 授权 Docker 文件
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
     sudo apt-get update
 
-    # 安装 Docker 的最新版本
+    # 安装 Docker 最新版本
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 else
-    echo "Docker 已经安装."
+    echo "Docker 已安装。"
 fi
 
 # 检查 Docker Compose 是否已安装
 if ! command -v docker-compose &> /dev/null
 then
-    echo "Docker Compose 未安装，执行安装..."
+    echo "未检测到 Docker Compose，正在安装..."
     sudo apt install docker-compose -y
 else
-    echo "Docker Compose 已经安装."
+    echo "Docker Compose 已安装。"
 fi
 
 # 验证 Docker Engine 安装是否成功
 sudo docker run hello-world
-# 你应该看到 hello-world 程序的输出
+# 应该能看到 hello-world 程序的输出
 
 # 检查 Docker Compose 版本
 docker-compose -v
@@ -88,14 +91,14 @@ docker-compose -v
 # 运行 Taiko 节点
 docker compose up -d
 
-# 进入网页查询
+# 获取公网 IP 地址
 public_ip=$(curl -s ifconfig.me)
 
-# 原始链接，其中的 LocalHost 将被替换
+# 准备原始链接
 original_url="LocalHost:3001/d/L2ExecutionEngine/l2-execution-engine-overview?orgId=1&refresh=10s"
 
 # 替换 LocalHost 为公网 IP 地址
 updated_url=$(echo $original_url | sed "s/LocalHost/$public_ip/")
 
 # 显示更新后的链接
-echo "请进入该链接查询设备运行情况，如果还无法进入，请等待2-3分钟 $updated_url"
+echo "请通过以下链接查询设备运行情况，如果无法访问，请等待2-3分钟后重试：$updated_url"
