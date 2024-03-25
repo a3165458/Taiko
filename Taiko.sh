c6f310243c06856c8b68434a14c40deab110e0ef
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-25 19:10:25 +0800

    Update Taiko.sh

--- Taiko.sh
+++ Taiko.sh
@@ -171,11 +171,11 @@ else
 fi
 
     # 安装 Docker compose 最新版本
-    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-    mkdir -p $DOCKER_CONFIG/cli-plugins
-    curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-    sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-    docker compose version
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version
 
 # 验证 Docker Engine 安装是否成功
 sudo docker run hello-world
