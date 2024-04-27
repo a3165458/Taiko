154c455705b4b2e069e5085b81a5ae4609e531f9
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-27 20:40:04 +0800

    Update Taiko.sh

--- Taiko.sh
+++ Taiko.sh
@@ -66,11 +66,17 @@ fi
 
 # 提示用户输入环境变量的值
 read -p "请输入BlockPI holesky HTTP链接: " l1_endpoint_http
+
 read -p "请输入BlockPI holesky WS链接: " l1_endpoint_ws
+
 read -p "请输入Beacon Holskey RPC（如果你没有搭建的话，请输入:https://ethereum-holesky-beacon-api.publicnode.com即可）链接: " l1_beacon_http
+
 read -p "请输入Prover RPC 链接(目前可用任意选一个:http://kenz-prover.hekla.kzvn.xyz:9876或者http://hekla.stonemac65.xyz:9876): " prover_endpoints
+
 read -p "请确认是否作为提议者（可选true或者false，目前prover 节点已经工作，请输入true，更新时间2024.4.26 15.30）: " enable_proposer
+
 read -p "请输入EVM钱包私钥,不需要带0x: " l1_proposer_private_key
+
 read -p "请输入EVM钱包地址: " l2_suggested_fee_recipient
 
 # 检测并罗列未被占用的端口
@@ -218,12 +224,19 @@ cd #HOME
 cd simple-taiko-node
 
 read -p "请输入BlockPI holesky HTTP链接: " l1_endpoint_http
+
 read -p "请输入BlockPI holesky WS链接: " l1_endpoint_ws
+
 read -p "请输入Beacon Holskey RPC（如果你没有搭建的话，请输入:https://ethereum-holesky-beacon-api.publicnode.com/即可）链接: " l1_beacon_http
+
 read -p "请输入Prover RPC 链接(目前可用任意选一个:http://kenz-prover.hekla.kzvn.xyz:9876或者http://hekla.stonemac65.xyz:9876): " prover_endpoints
+
 read -p "请确认是否作为提议者（可选true或者false，目前prover 节点已经工作，请输入true，更新时间2024.4.26 15.30）: " enable_proposer
+
 read -p "请确认是否关闭P2P同步（可选true或者false，请选择false开启）: " disable_p2p_sync
+
 read -p "请输入EVM钱包私钥: " l1_proposer_private_key
+
 read -p "请输入EVM钱包地址: " l2_suggested_fee_recipient
 
 # 将用户输入的值写入.env文件
